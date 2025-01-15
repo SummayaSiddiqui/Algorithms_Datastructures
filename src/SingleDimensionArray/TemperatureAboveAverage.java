4647a0ae8b58b68678b487cf37c4d7a9e5ccaddb
SummayaSiddiqui <summaya.siddiqui@keyin.com>
2025-01-15 10:32:05 -0330

    Corrected spelling of e.g.

--- src/SingleDimensionArray/TemperatureAboveAverage.java
+++ src/SingleDimensionArray/TemperatureAboveAverage.java
@@ -1,5 +1,5 @@
 /* Calculate the average temperature, and give how many days are above the average temperature
-* 1. Take an input from the user (eg. 5)
+* 1. Take an input from the user (e.g. 5)
 * 2. Prompt the user to enter all the 5 numbers (temperature values)
 * 3. Calculate the average temperature
 * 4. Given the average temperature, how many of the numbers in the line 2 are above the avarage temperature?
