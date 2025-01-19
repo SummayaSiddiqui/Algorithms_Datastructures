5a866f23190e7d7fa7fc464d180f3701950cc402
SummayaSiddiqui <summaya.siddiqui@keyin.com>
2025-01-19 13:10:44 -0330

    Added a method to display seating chart

--- src/DoubleDimensionArray/MovieTheatreImplementation.java
+++ src/DoubleDimensionArray/MovieTheatreImplementation.java
@@ -23,17 +23,31 @@ public class MovieTheatreImplementation {
         }
     }
 
-    public void insert (char r, int c, int seatNumber) {
-        try {
-            int rowName = r - 'A';
-            if (arr[rowName][c] == Integer.MIN_VALUE){
-                arr[rowName][c] = seatNumber;
-                System.out.println("Successfully inserted");
-            } else {
-                System.out.println("The location is already occupied");
+//    public void insert (char r, int c, int seatNumber) {
+//        try {
+//            int rowName = r - 'A';
+//            if (arr[rowName][c] == Integer.MIN_VALUE){
+//                arr[rowName][c] = seatNumber;
+//                System.out.println("Successfully inserted");
+//            } else {
+//                System.out.println("The location is already occupied");
+//            }
+//        } catch (ArrayIndexOutOfBoundsException e) {
+//            System.out.println("Invalid index for 2D array");
+//        }
+//    }
+
+//    Method to display the seating chart
+    public void displaySeatingChart() {
+        for(int r = 0; r < arr.length; r++) {
+            for(int c = 0 ; c < arr[r].length; c++) {
+                if (arr[r][c] == Integer.MIN_VALUE) {
+                    System.out.print("O ");
+                } else {
+                    System.out.print("X ");
+                }
             }
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Invalid index for 2D array");
+            System.out.println();
         }
     }
 
@@ -41,7 +55,30 @@ public class MovieTheatreImplementation {
 //        if there are 10 rows and 10 seats in each row
         MovieTheatreImplementation seat = new MovieTheatreImplementation('K',10);
         System.out.println("Welcome to the seat booking app.");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("To view seating chart type 1");
+            System.out.println("To reserve seat type 2");
+            System.out.println("To exit the system type 3");
 
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            if (choice == 1) {
+                seat.displaySeatingChart();
+                System.out.println();
+            } else if (choice == 2) {
+                System.out.println();
+            } else if (choice == 3) {
+                System.out.println("Thank you for using the app!");
+                break;
+            } else {
+                System.out.println("Invalid choice. Please try again.");
+            }
+        }
+        scanner.close();
+
+        }
     }
 
-}
+
